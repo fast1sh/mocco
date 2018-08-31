62a3fb12e71b722f373fde509139f457cb70ad4e
fast1sh <dlemeshko04@gmail.com>
2018-09-01 01:04:41 +0300

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,13 @@
 window.onscroll = function () { myFunction() };
 var navbar = document.getElementById("nav");
+var main = document.getElementById("main");
 var sticky = nav.offsetTop;
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("fixed")
+    navbar.classList.add("fixed");
+    main.classList.add("fixed-main");
   } else {
     navbar.classList.remove("fixed");
+    main.classList.remove("fixed-main");
   }
 };
