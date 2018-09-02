c5f7f8fcc97368ee2174c405717df60a9616a5bc
fast1sh <dlemeshko04@gmail.com>
2018-09-02 22:20:55 +0300

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Sticky menu
 window.onscroll = function () { myFunction() };
 var navbar = document.getElementById("nav");
 var main = document.getElementById("main");
@@ -11,3 +12,11 @@ function myFunction() {
     main.classList.remove("fixed-main");
   }
 };
+// Convenience slider
+$('.convenience-slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  prevArrow: '<button class="convenience-slider__prev convenience-slider__arrow"><i class="fas fa-chevron-circle-left"></i></button>',
+  nextArrow: '<button class="convenience-slider__next convenience-slider__arrow"><i class="fas fa-chevron-circle-right"></i></button>'
+});
